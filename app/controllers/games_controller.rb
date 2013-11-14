27b8f39e7cdfef5e907bcc5d3e7cf150ebc58e98
etomlef <etomlef@gmail.com>
2013-11-13 19:55:08 -0500

    Deleted debug stuff, getting ready for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,4 @@ class GamesController < ApplicationController
   def index
   end
 
-  def debug
-    @not_special = params["not_special"]
-    # the only way to pass params from a 
-    # controller to a view is through instance variable
-  end
 end
