7acbfeb74d08ccd3889dbeffe6f79ca144217bb7
etomlef <etomlef@gmail.com>
2013-11-13 20:48:34 -0500

    Working Secret Number

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GamingApp::Application.routes.draw do
+  root "games#index" # root defines the homepage
   get "games", to: 'games#index'  # when you get games, go to the index
-
+  get "games/secret_number", to: 'games#secret_number'
+  get "games/secret_number/:guess", to: 'games#secret_number_play'
 end
